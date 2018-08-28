4edc6c2b6121928bb2e587274d3ec7072b3350f0
pilar <sauleturme@gmail.com>
2018-08-28 11:07:31 +0200

    Added redux, created our first component Shop.jsx

---
+++ src/components/Shop.jsx
@@ -0,0 +1,14 @@
+import React, {Component, Fragment} from 'react';
+
+class Shop extends Component {
+
+  render(){
+    return (
+      <Fragment>
+        <p>Test</p>
+      </Fragment>
+    );
+  }
+}
+
+export default Shop;
