a27e0591f4e6d5ec1199441b70920689c7e35d7b
pilar <sauleturme@gmail.com>
2018-08-28 11:47:36 +0200

    Imported json file to our index.js file

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,1 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class ProductList extends Component {
+  render() {
+    return(
+      <ul>
+        <li>
+          <p></p>
+        </li>
+        <li>
+          <p></p>
+        </li>
+        <li>
+          <p></p>
+        </li>
+      </ul>
+    )
+  }
+}
+
+export default ProductList;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import products from './products.json';
+
+// this is a mock Reducer
+const appReducer = state => state;
+
+const store = createStore(appReducer, {
+  products: products.reduce((products, product) => ({
+    ...products,
+    [product.id]: product
+  }), {}),
+  cart: {}
+}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
